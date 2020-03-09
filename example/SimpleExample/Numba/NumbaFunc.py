233fdbed2f2813c1de89e9ae1ca47581d724f15e
Andrey Grabovoy <grabovoy@grabovoy.ap-team.ru>
2020-03-10 01:55:14 +0300

    fixed numba

--- example/SimpleExample/Numba/NumbaFunc.py
+++ example/SimpleExample/Numba/NumbaFunc.py
@@ -6,10 +6,3 @@ def NumbaFunc(N):
     for n in range(N):
         ret += n
     return ret
-
-@njit(parallel=True)
-def NumbaOpenMPFunc(N):
-    ret = 0
-    for n in prange(N):
-        ret += n
-    return ret
